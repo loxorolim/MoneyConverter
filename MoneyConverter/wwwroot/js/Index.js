2ab7ddc960084c4315b455901b600b8de291b32a
Guilherme Rolim <grolim@outlook.com.br>
2017-12-21 10:03:32 -0200

    Adiciona chamada teste de Get via JQuery.

--- MoneyConverter/wwwroot/js/Index.js
+++ MoneyConverter/wwwroot/js/Index.js
@@ -5,7 +5,7 @@
     self.value = value;
 
     self.formattedCurrency = function () {
-        return self.currencySymbol + self.value.toString();
+        return self.currencySymbol + " " + self.value.toString();
     };  
 }
 
@@ -15,10 +15,21 @@ var indexViewModel = new function IndexViewModel () {
 
     self.quotations = [
         new Quotation("Euro (EUR)", "€", 123),
-        new Quotation("Euro (EUR)", "€", 123),
-        new Quotation("Euro (EUR)", "€", 123),
-        new Quotation("Euro (EUR)", "€", 123)
+        new Quotation("Great Britain Pound (GBP)", "£", 123),
+        new Quotation("Japan Yen (JPY)", "¥", 123),
+        new Quotation("Yuan (CNH)", "元", 123)
     ]; 
+
+    self.updateValues = function () {
+
+        $.get(
+            "https://forex.1forge.com/1.0.2/quotes?pairs=EURUSD,GBPJPY,AUDUSD&api_key=YOUR_API_KEY",
+            function (data) {
+                alert(data);
+            }
+        );
+
+    }
 
 }
 
