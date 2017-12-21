47b354d7aeea1f4291fdd9e11541e45a7211b527
Guilherme Rolim <grolim@outlook.com.br>
2017-12-20 23:19:47 -0200

    Inicia projeto.

--- MoneyConverter/Pages/CircularListChallenge.cshtml.cs
+++ MoneyConverter/Pages/CircularListChallenge.cshtml.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MoneyConverter.Pages
 {
-    public class ContactModel : PageModel
+    public class CircularListChallengeModel : PageModel
     {
         public string Message { get; set; }
 
