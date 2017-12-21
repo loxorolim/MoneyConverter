1cfd65a2a7bedc31206efe74052e6ea7fd911829
Guilherme Rolim <grolim@outlook.com.br>
2017-12-21 09:29:00 -0200

    Adiciona knockoutJs.

---
+++ MoneyConverter/wwwroot/js/Index.js
@@ -0,0 +1,26 @@
+﻿function Quotation(name, symbol, value) {
+    var self = this;
+    self.currencyName = name;
+    self.currencySymbol = symbol;
+    self.value = value;
+
+    self.formattedCurrency = function () {
+        return self.currencySymbol + self.value.toString();
+    };  
+}
+
+var indexViewModel = new function IndexViewModel () {
+
+    var self = this;
+
+    self.quotations = [
+        new Quotation("Euro (EUR)", "€", 123),
+        new Quotation("Euro (EUR)", "€", 123),
+        new Quotation("Euro (EUR)", "€", 123),
+        new Quotation("Euro (EUR)", "€", 123)
+    ]; 
+
+}
+
+// Activates knockout.js
+ko.applyBindings(indexViewModel);
