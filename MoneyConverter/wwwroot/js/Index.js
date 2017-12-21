d112b94d6529bdced2354481e85b28927a2cb86d
Guilherme Rolim <grolim@outlook.com.br>
2017-12-21 21:57:47 -0200

    Adiciona refresh das cotações e permite escolha de moeda para conversão de valor.

--- MoneyConverter/wwwroot/js/Index.js
+++ MoneyConverter/wwwroot/js/Index.js
@@ -1,11 +1,17 @@
-﻿function Quotation(name, symbol, value) {
+﻿function Quotation(name, abbreviation, symbol, value) {
     var self = this;
     self.currencyName = name;
+    self.currencyAbbreviation = abbreviation;
     self.currencySymbol = symbol;
-    self.value = value;
+    self.value = ko.observable(value);
+
+
+    self.fullName = function () {
+        return self.currencyName + " (" + self.currencyAbbreviation + ")" 
+    };
 
     self.formattedCurrency = function () {
-        return self.currencySymbol + " " + self.value.toString();
+        return self.currencySymbol + " " + self.value().toString();
     };  
 }
 
@@ -13,24 +19,33 @@ var indexViewModel = new function IndexViewModel () {
 
     var self = this;
 
-    self.quotations = [
-        new Quotation("Euro (EUR)", "€", 123),
-        new Quotation("Great Britain Pound (GBP)", "£", 123),
-        new Quotation("Japan Yen (JPY)", "¥", 123),
-        new Quotation("Yuan (CNH)", "元", 123)
-    ]; 
+    self.inputAmount = ko.observable();
+
+    self.quotations = ko.observableArray([
+        new Quotation("Euro", "EUR", "€", 0),
+        new Quotation("Great Britain Pound","GBP", "£", 0),
+        new Quotation("Japan Yen", "JPY", "¥", 0),
+        new Quotation("Yuan", "CNH", "元", 0)
+    ]); 
+
+    self.inputQuotation = ko.observable(self.quotations()[0]);
+    self.outputQuotation = ko.observable(self.quotations()[0]);
 
     self.updateValues = function () {
 
         $.get(
-            "https://forex.1forge.com/1.0.2/quotes?pairs=EURUSD,GBPJPY,AUDUSD&api_key=YOUR_API_KEY",
+            "https://forex.1forge.com/1.0.2/quotes?pairs=USDEUR,USDGBP,USDJPY,USDCNH&api_key=Yer9o3wICwhKlviLU0zZGavH9AGZuqd3",
             function (data) {
-                alert(data);
+                for (i = 0; i < data.length; i++) {
+                    self.quotations()[i].value(data[i].price);
+                }
             }
         );
-
     }
 
+    self.updateValues();
+    setInterval(self.updateValues, 30000);
+    
 }
 
 // Activates knockout.js
